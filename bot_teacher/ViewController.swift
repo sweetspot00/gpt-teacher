a903e6353dbe42115315fd15db5d37243b804c7d
sweetspot00 <50688000+sweetspot00@users.noreply.github.com>
2023-03-04 22:26:43 +0800

    this is the first commit

---
+++ bot_teacher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bot_teacher
+//
+//  Created by niwanchun on 2023/3/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bot_teacherUITests/bot_teacherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bot_teacherUITestsLaunchTests.swift
+//  bot_teacherUITests
+//
+//  Created by niwanchun on 2023/3/4.
+//
+
+import XCTest
+
+final class bot_teacherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
