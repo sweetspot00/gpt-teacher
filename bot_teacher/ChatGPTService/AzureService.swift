7bb8c95496ac2beb5bf723de807f6f94a7c3b057
Muneeb ur Rahman <muneebdev1@gmail.com>
2023-03-20 23:21:34 +0500

    Update AzureService.swift

--- bot_teacher/ChatGPTService/AzureService.swift
+++ bot_teacher/ChatGPTService/AzureService.swift
@@ -16,23 +16,55 @@ class AzureSerivce {
     
     var inputText = ""
     
-    var speakerName: String!
-    var synthesizer = SPXSpeechSynthesizer()
-    
-    func config(with name: String) {
+    var speakerName: String
+    var rate: Float?
+    var pitch: Float?
+
+    func config(with name: String, rate: Float? = nil, pitch: Float? = nil) {
         self.speakerName = name
+        self.rate = rate
+        self.pitch = pitch
     }
     
     func changeInputTextAndPlay(with input: String) {
         inputText = input
-        self.synthesisToSpeaker()
+        self.synthesize()
     }
-    
+    func sendRequest() {
+    let url = URL(string: "https://speech-functions-cfgfd3fyb8gmhwbj.z01.azurefd.net/api/synthesize")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    let parameters: [String: Any] = ["text": inputText, "voice": speakerName]
+    if rate != nil {
+        parameters["rate"] = rate
+    }
+    if pitch != nil {
+        parameters["pitch"] = pitch
+    }
+    let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+    request.httpBody = jsonData
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            if let file = responseJSON["file"] as? String {
+                        DispatchQueue.main.async {
+                            self.playSound(file: file)
+                        }
+            }
+        }
+    }
+    task.resume()
+    }
 
-    func synthesisToSpeaker() {
-        
+    func playSound(file: String) {
+        let dataDecoded : Data = Data(base64Encoded: file, options: .ignoreUnknownCharacters)!
+        let decodedString = String(data: dataDecoded, encoding: .utf8)!
         let audioSession = AVAudioSession.sharedInstance()
-
         do {
             try audioSession.setCategory(AVAudioSession.Category.playback)
             try audioSession.setActive(true)
@@ -44,39 +76,14 @@ class AzureSerivce {
 
         audioEngine.attach(playerNode)
         let audioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 16000, channels: 1, interleaved: false)
-
         audioEngine.connect(playerNode, to: audioEngine.mainMixerNode, format: audioFormat)
-
-        // MARK: Azure synthesizer config
-        var speechConfig: SPXSpeechConfiguration?
+        playerNode.scheduleBuffer(dataDecoded, at: nil, options: .loops, completionHandler: nil)
         do {
-            try speechConfig = SPXSpeechConfiguration(subscription: AZURE_KEY, region: AZURE_REGION)
-        } catch {
-            print("error \(error) happened")
-            speechConfig = nil
+            try audioEngine.start()
         }
-        
-        speechConfig?.speechSynthesisVoiceName = speakerName;
-        synthesizer = try! SPXSpeechSynthesizer(speechConfig!)
-
-        
-        if inputText.isEmpty {
-            return
+         catch {
+            print("Error starting audio engine: \(error.localizedDescription)")
         }
-        
-        
-        let result = try! synthesizer.speakText(inputText)
-        
-        
-        if result.reason == SPXResultReason.canceled
-        {
-            let cancellationDetails = try! SPXSpeechSynthesisCancellationDetails(fromCanceledSynthesisResult: result)
-            print("cancelled, detail: \(cancellationDetails.errorDetails!) ")
-            try! audioSession.setActive(false)
-        }
-        // stop audioSession
-       
+        playerNode.play()
     }
-    
-    
 }
