a236dfc87f0eeac7178496233b703e09dd79686f
Muneeb ur Rahman <muneebdev1@gmail.com>
2023-03-21 01:18:12 +0500

    Update AzureService.swift

--- bot_teacher/ChatGPTService/AzureService.swift
+++ bot_teacher/ChatGPTService/AzureService.swift
@@ -9,14 +9,13 @@
 //
 
 // <code>
-import MicrosoftCognitiveServicesSpeech
 import AVFoundation
 
-class AzureSerivce {
+class AzureService {
     
     var inputText = ""
     
-    var speakerName: String
+    var speakerName: String = ""
     var rate: Float?
     var pitch: Float?
 
@@ -28,62 +27,65 @@ class AzureSerivce {
     
     func changeInputTextAndPlay(with input: String) {
         inputText = input
-        self.synthesize()
+        self.sendRequest()
     }
+    
     func sendRequest() {
-    let url = URL(string: "https://speech-functions-cfgfd3fyb8gmhwbj.z01.azurefd.net/api/synthesize")!
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    let parameters: [String: Any] = ["text": inputText, "voice": speakerName]
-    if rate != nil {
-        parameters["rate"] = rate
-    }
-    if pitch != nil {
-        parameters["pitch"] = pitch
-    }
-    let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
-    request.httpBody = jsonData
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        guard let data = data, error == nil else {
-            print(error?.localizedDescription ?? "No data")
-            return
+        let url = URL(string: "https://speech-functions-cfgfd3fyb8gmhwbj.z01.azurefd.net/api/synthesize")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let parameters: [String: Any] = ["text": inputText, "voice": speakerName]
+        if rate != nil {
+            parameters["rate"] = rate
         }
-        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-        if let responseJSON = responseJSON as? [String: Any] {
-            if let file = responseJSON["file"] as? String {
-                        DispatchQueue.main.async {
-                            self.playSound(file: file)
-                        }
+        if pitch != nil {
+            parameters["pitch"] = pitch
+        }
+        let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any], let file = responseJSON["file"] as? String {
+                DispatchQueue.main.async {
+                    self.playSound(file: file)
+                }
             }
         }
-    }
-    task.resume()
+        task.resume()
     }
 
     func playSound(file: String) {
-        let dataDecoded : Data = Data(base64Encoded: file, options: .ignoreUnknownCharacters)!
-        let decodedString = String(data: dataDecoded, encoding: .utf8)!
+        let dataDecoded = Data(base64Encoded: file)!
         let audioSession = AVAudioSession.sharedInstance()
         do {
-            try audioSession.setCategory(AVAudioSession.Category.playback)
-            try audioSession.setActive(true)
+            try audioSession.setCategory(.playAndRecord, options: .defaultToSpeaker)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
         } catch {
             print("Error setting audio session category: \(error.localizedDescription)")
         }
         let audioEngine = AVAudioEngine()
         let playerNode = AVAudioPlayerNode()
-
         audioEngine.attach(playerNode)
         let audioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 16000, channels: 1, interleaved: false)
+        guard let audioFile = try? AVAudioFile(forReading: dataDecoded) else {
+            print("Error creating audio file from data")
+            return
+        }
         audioEngine.connect(playerNode, to: audioEngine.mainMixerNode, format: audioFormat)
-        playerNode.scheduleBuffer(dataDecoded, at: nil, options: .loops, completionHandler: nil)
+        playerNode.scheduleFile(audioFile, at: nil) {
+            audioEngine.stop()
+            audioEngine.reset()
+        }
         do {
             try audioEngine.start()
-        }
-         catch {
+        } catch {
             print("Error starting audio engine: \(error.localizedDescription)")
         }
         playerNode.play()
-    }
+}
 }
