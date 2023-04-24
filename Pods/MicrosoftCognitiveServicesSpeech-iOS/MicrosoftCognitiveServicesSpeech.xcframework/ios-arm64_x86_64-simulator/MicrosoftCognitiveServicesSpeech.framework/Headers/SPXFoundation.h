65caa033c4fc1a046d260a1e8a1fab9dfbd2e9b9
sweetspot00 <50688000+sweetspot00@users.noreply.github.com>
2023-04-24 19:06:17 +0800

    update: gitignore file

--- Pods/MicrosoftCognitiveServicesSpeech-iOS/MicrosoftCognitiveServicesSpeech.xcframework/ios-arm64_x86_64-simulator/MicrosoftCognitiveServicesSpeech.framework/Headers/SPXFoundation.h
+++
@@ -1,9 +0,0 @@
-///
-// Copyright (c) Microsoft. All rights reserved.
-// See https://aka.ms/csspeech/license for the full license information.
-//
-
-#import <Foundation/Foundation.h>
-
-#define SPX_EXPORT __attribute__((visibility ("default")))
-
--- Pods/Target Support Files/Pods-bot_teacher/Pods-bot_teacher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bot_teacher : NSObject
-@end
-@implementation PodsDummy_Pods_bot_teacher
-@end
